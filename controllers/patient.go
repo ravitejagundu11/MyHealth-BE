8363d03e71f3860adfe9e2757bc48428184cf0ae
Yashada7 <yashadatembe07@gmail.com>
2025-11-06 18:15:52 -0500

    controller files for doctor and patient

---
+++ controllers/patient.go
@@ -0,0 +1,13 @@
+type patient struct{
+	patientId string
+}
+
+func newpatient(patientId string) *patient{
+	return &patient{}
+}
+
+// Book an appointment with the doctor
+func BookAppointment(doctorId string, dateTime string){
+	// TODO: return appointmnet struct created in appointments controller
+	// Update return type
+}
